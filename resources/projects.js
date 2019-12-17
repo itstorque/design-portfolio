03e27de68d952e8fe383b9e94ab50c086e8792a1
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-12-17 16:28:45 -0500

    Added tile support to projects page and modified some styles+scripts

--- resources/projects.js
+++ resources/projects.js
@@ -1,11 +1,13 @@
-var mouseX = 0, mouseY = 0;
-
 $(document).ready(function() {
   var xp = 0, yp = 0;
+
+  var mouseX = 0
+  var mouseY = 0
 
   $(document).mousemove(function(e){
     mouseX = e.pageX - 30;
     mouseY = e.pageY - 30;
+    randomizer_modifier = (((mouseX- window.innerWidth/2)/window.innerWidth)**2 - ((mouseY- window.innerHeight/2)/window.innerHeight)**2)*20
   });
 
   setInterval(function(){
@@ -13,5 +15,10 @@ $(document).ready(function() {
     yp += ((mouseY - yp)/3);
     $("#cursor").css({left: xp +'px', top: yp +'px'});
   }, 20);
+
+  $(document).scroll(function() {
+    $("#titleline1").html("WEB");
+    $("#titleline2").html("DESIGN");
+  });
 
 });
